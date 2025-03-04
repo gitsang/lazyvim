36c921f13ef6c4fc46e78921d4868d15a4075f06
sang <sang.chen@outlook.com>
2025-03-04 18:18:35 +0800

    Update

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,54 @@
+return {
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false,
+    opts = {
+      provider = "worklink",
+      auto_suggestions_provider = "worklink",
+      vendors = {
+        worklink = {
+          __inherited_from = "openai",
+          endpoint = "https://worklink.yealink.com/llmproxy",
+          model = "gpt-4o",
+          api_key_name = "WORKLINK_API_KEY",
+        },
+      },
+    },
+    build = "make",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "echasnovski/mini.pick", -- for file_selector provider mini.pick
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
+}
---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,104 @@
+local config = loadfile(os.getenv("HOME") .. "/.config/nvim/lua/plugins.conf/codecompanion.lua")()
+
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "hrsh7th/nvim-cmp", -- Optional: For using slash commands and variables in the chat buffer
+      "nvim-telescope/telescope.nvim", -- Optional: For using slash commands
+      { -- Optional: For prettier markdown rendering
+        "MeanderingProgrammer/render-markdown.nvim",
+        ft = { "markdown", "codecompanion" },
+      },
+      { -- Optional: Improves `vim.ui.select`
+        "stevearc/dressing.nvim",
+        opts = {},
+      },
+    },
+    opts = {
+      log_level = "DEBUG",
+      language = "Chinese",
+      strategies = {
+        chat = {
+          adapter = "worklink",
+          keymaps = {
+            send = {
+              modes = { n = "<C-s>", i = "<C-s>" },
+            },
+            close = {
+              modes = { n = "<C-c>", i = "<C-c>" },
+            },
+          },
+          slash_commands = {
+            ["file"] = {
+              callback = "strategies.chat.slash_commands.file",
+              description = "Select a file using Telescope",
+              opts = {
+                provider = "telescope", -- Other options include 'default', 'mini_pick', 'fzf_lua', snacks
+                contains_code = true,
+              },
+            },
+            ["git_files"] = {
+              description = "List git files",
+              ---@param chat CodeCompanion.Chat
+              callback = function(chat)
+                local handle = io.popen("git ls-files")
+                if handle ~= nil then
+                  local result = handle:read("*a")
+                  handle:close()
+                  chat:add_reference({ content = result }, "git", "<git_files>")
+                else
+                  return vim.notify("No git files available", vim.log.levels.INFO, { title = "CodeCompanion" })
+                end
+              end,
+              opts = {
+                contains_code = false,
+              },
+            },
+          },
+        },
+        inline = {
+          adapter = "worklink",
+        },
+      },
+      adapters = {
+        worklink = function()
+          return require("codecompanion.adapters").extend("openai_compatible", {
+            name = "worklink",
+            env = {
+              url = config.url,
+              api_key = config.api_key,
+              chat_url = "/v1/chat/completions",
+              models_endpoint = "/v1/models",
+            },
+            schema = {
+              model = {
+                default = "gpt-4o",
+              },
+            },
+          })
+        end,
+      },
+      display = {
+        chat = {
+          intro_message = "Welcome to CodeCompanion ✨! Press ? for options",
+          show_header_separator = false, -- Show header separators in the chat buffer? Set this to false if you're using an external markdown formatting plugin
+          separator = "─", -- The separator between the different messages in the chat buffer
+          show_references = true, -- Show references (from slash commands and variables) in the chat buffer?
+          show_settings = true, -- Show LLM settings at the top of the chat buffer?
+          show_token_count = true, -- Show the token count for each response?
+          start_in_insert_mode = false, -- Open the chat buffer in insert mode?
+        },
+        diff = {
+          enabled = true,
+          close_chat_at = 240, -- Close an open chat buffer if the total columns of your display are less than...
+          layout = "vertical", -- vertical|horizontal split for default provider
+          opts = { "internal", "filler", "closeoff", "algorithm:patience", "followwrap", "linematch:120" },
+          provider = "default", -- default|mini_diff
+        },
+      },
+    },
+  },
+}
