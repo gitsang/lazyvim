92777d2284ecd2d6ac3859f6f68d6a8982029670
sang <sang.chen@outlook.com>
2025-03-05 21:08:24 +0800

    update(auto): auto commit
    MODIFIED:
    	lua/plugins/codecompanion.lua

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -71,6 +71,15 @@ return {
         end,
       },
       display = {
+        chat = {
+          intro_message = "Welcome to CodeCompanion ✨! Press ? for options",
+          show_header_separator = false, -- Show header separators in the chat buffer? Set this to false if you're using an external markdown formatting plugin
+          separator = "─", -- The separator between the different messages in the chat buffer
+          show_references = true, -- Show references (from slash commands and variables) in the chat buffer?
+          show_settings = true, -- Show LLM settings at the top of the chat buffer?
+          show_token_count = true, -- Show the token count for each response?
+          start_in_insert_mode = false, -- Open the chat buffer in insert mode?
+        },
         diff = {
           enabled = true,
           close_chat_at = 240, -- Close an open chat buffer if the total columns of your display are less than...
