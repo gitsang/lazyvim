4a773e4f8db6a5bba5067606a0c2b573fbc34ee4
sang <sang.chen@outlook.com>
2025-03-06 12:37:40 +0800

    update(auto): auto commit
    MODIFIED:
    	lua/config/keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,4 @@ local map = LazyVim.safe_keymap_set
 
 map("n", ";", "$", { desc = "Go to End of Line", remap = true })
 map("x", ";", "$", { desc = "Go to End of Line", remap = true })
+map("o", ";", "$", { desc = "Go to End of Line", remap = true })
