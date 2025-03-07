6231837d1ec311ecbf37a6590e627a329c3eb5ed
sang <sang.chen@outlook.com>
2025-03-07 18:09:48 +0800

    update(auto): auto commit
    NEW:
    	lua/plugins/codeium.lua

---
+++ lua/plugins/codeium.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "Exafunction/codeium.vim",
+    branch = "main",
+    config = function()
+      vim.g.codeium_no_map_tab = 1
+      vim.keymap.set("i", "<C-g>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true, script = true, silent = true, nowait = true })
+      vim.keymap.set("i", "<C-;>", "<Cmd>call codeium#CycleCompletions(1)<CR>", {})
+      vim.keymap.set("i", "<C-,>", "<Cmd>call codeium#CycleCompletions(-1)<CR>", {})
+      vim.keymap.set("i", "<C-x>", "<Cmd>call codeium#Clear()<CR>", {})
+    end,
+  },
+}
