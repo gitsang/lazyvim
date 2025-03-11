5726f99f2d3ee1ff0a033984b25a3ed7bac3edd4
sang <sang.chen@outlook.com>
2025-03-11 19:12:46 +0800

    update(auto): auto commit
    MODIFIED:
    	lua/config/keymaps.lua
    
    DELETED:
    	lua/plugins/tab-autocomplete.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,11 +11,12 @@ local function keymap(modes, key, command, opts)
 end
 
 keymap({ "n", "x", "o" }, ";", "$", { desc = "Go to End of Line", remap = true })
-keymap({ "n", "x", "o" }, "<leader>cc", "<Cmd>CodeCompanionChat<CR>", { desc = "Toggle Chat", remap = true })
-keymap({ "n", "x", "o" }, "<leader>aa", "<Cmd>CodeCompanionAction<CR>", { desc = "Prompt Actions", remap = true })
 
 if vim.o.wildoptions:match("pum") then
   vim.api.nvim_set_keymap("c", "<Up>", [[pumvisible() ? '<Left>' : '<Up>']], { expr = true })
   vim.api.nvim_set_keymap("c", "<Down>", [[pumvisible() ? '<Right>' : '<Down>']], { expr = true })
-  vim.api.nvim_set_keymap("c", "<CR>", [[pumvisible() ? '<C-y>' : '<CR>']], { expr = true })
+  vim.api.nvim_set_keymap("c", "<Right>", [[pumvisible() ? '<C-y>' : '<Right>']], { expr = true })
 end
+
+keymap({ "n", "x", "o" }, "<leader>A", "<Cmd>CodeCompanionAction<CR>", { desc = "Prompt Actions", remap = true })
+keymap({ "n", "x", "o" }, "<leader>C", "<Cmd>CodeCompanionChat<CR>", { desc = "Toggle Chat", remap = true })
