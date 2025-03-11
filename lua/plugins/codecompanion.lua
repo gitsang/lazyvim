f8d7dfac76e889c769d25039666cc0dd5f436b52
sang <sang.chen@outlook.com>
2025-03-11 10:26:05 +0800

    update(auto): auto commit
    MODIFIED:
    	lua/plugins/codecompanion.lua

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -31,8 +31,12 @@ return {
       },
     },
 
-    keymap({ "n", "v", "x" }, configs.keys.toggle_chat, function() end),
-    keymap({ "n", "v", "x" }, configs.keys.prompt_actions, "<Cmd>CodeCompanionAction<CR>"),
+    -- local map = LazyVim.safe_keymap_set
+    -- map("n", ";", "$", { desc = "Go to End of Line", remap = true })
+    -- map("x", ";", "$", { desc = "Go to End of Line", remap = true })
+    -- map("o", ";", "$", { desc = "Go to End of Line", remap = true })
+    -- keymap({ "n", "v", "x" }, configs.keys.toggle_chat, function() end),
+    -- keymap({ "n", "v", "x" }, configs.keys.prompt_actions, "<Cmd>CodeCompanionAction<CR>"),
 
     opts = {
       opts = {
