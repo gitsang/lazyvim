4e022212c0cf3e52ff3990ad6c675e93508ab679
sang <sang.chen@outlook.com>
2025-03-12 11:44:51 +0800

    update(auto): auto commit
    MODIFIED:
    	lua/plugins/codeium.lua
    	lua/plugins/completion.lua
    
    NEW:
    	lua/plugins/minuet-ai.lua

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -7,46 +7,63 @@ local has_words_before = function()
 end
 
 return {
-  "hrsh7th/nvim-cmp",
-  opts = function(_, opts)
-    opts.preselect = cmp.PreselectMode.None
-    opts.completion = { completeopt = "menu,menuone,noselect" }
-    opts.mapping = vim.tbl_extend("force", opts.mapping, {
-      ["<Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        elseif vim.snippet.active({ direction = 1 }) then
-          vim.schedule(function()
-            vim.snippet.jump(1)
-          end)
-        elseif has_words_before() then
-          cmp.complete()
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-      ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif vim.snippet.active({ direction = -1 }) then
-          vim.schedule(function()
-            vim.snippet.jump(-1)
-          end)
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-      ["<CR>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          if cmp.get_active_entry() then
-            cmp.confirm({ select = true })
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-emoji",
+    },
+    opts = function(_, opts)
+      opts.preselect = cmp.PreselectMode.None
+      opts.completion = { completeopt = "menu,menuone,noselect" }
+      -- table.insert(opts.sources, 1, {
+      --   name = "minuet",
+      --   group_index = 1,
+      --   priority = 100,
+      -- })
+      opts.performance = {
+        fetching_timeout = 2000,
+      }
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif vim.snippet.active({ direction = 1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(1)
+            end)
+          elseif has_words_before() then
+            cmp.complete()
           else
             fallback()
           end
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-    })
-  end,
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif vim.snippet.active({ direction = -1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(-1)
+            end)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<CR>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            if cmp.get_active_entry() then
+              cmp.confirm({ select = true })
+            else
+              fallback()
+            end
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<A-y>"] = require("minuet").make_cmp_map(),
+      })
+    end,
+  },
 }
