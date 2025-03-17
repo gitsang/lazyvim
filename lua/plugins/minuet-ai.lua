3bc77f571f7487f39a4b43a061d53096c23cf9a1
sang <sang.chen@outlook.com>
2025-03-17 09:56:16 +0800

    update(auto): auto commit
    MODIFIED:
    	lua/plugins/minuet-ai.lua
    
    NEW:
    	lua/vars/minuet-ai.lua

--- lua/plugins/minuet-ai.lua
+++ lua/plugins/minuet-ai.lua
@@ -1,57 +1,4 @@
-local secret = require("vars.secret")
-
-local default_stop = {
-  "<|endoftext|>",
-  "<|fim_prefix|>",
-  "<|fim_middle|>",
-  "<|fim_suffix|>",
-  "<|fim_pad|>",
-  "<|repo_name|>",
-  "<|file_sep|>",
-  "<|im_start|>",
-  "<|im_end|>",
-  "/src/",
-  "#- coding: utf-8",
-  "```",
-  "\n\n",
-}
-
-local default_optional = {
-  max_tokens = 4096,
-  temperature = 0.01,
-  top_p = 0.2,
-  provider = {
-    sort = "throughput",
-  },
-  num_predict = 4096,
-  num_ctx = 8192,
-  stop = default_stop,
-}
-
-local function openai_fim_compatible(name, end_point, api_key, model, stream, optional)
-  return {
-    name = name,
-    end_point = end_point,
-    api_key = function()
-      return api_key
-    end,
-    model = model,
-    stream = stream,
-    optional = optional,
-  }
-end
-
-local worklink_endpoint = "https://worklink.yealink.com/llmproxy/v1/completions"
-local worklink_api_key = secret.worklink_llm
-local function worklink_openai_fim_compatible(name, model)
-  return openai_fim_compatible(name, worklink_endpoint, worklink_api_key, model, true, default_optional)
-end
-
-local aihubmix_endpoint = "https://aihubmix.com"
-local aihubmix_api_key = secret.aihubmix
-local function aihubmix_openai_fim_compatible(name, model)
-  return openai_fim_compatible(name, aihubmix_endpoint, aihubmix_api_key, model, true, default_optional)
-end
+local minuet = require("vars.minuet-ai")
 
 return {
   {
@@ -65,8 +12,7 @@ return {
         debounce = 600, -- Increase to reduce costs and avoid rate limits
         notify = "verbose",
         provider_options = {
-          -- openai_fim_compatible = worklink_openai_fim_compatible("qwen2.5-coder:32b", "qwen2.5-coder-32b-instruct"),
-          openai_fim_compatible = aihubmix_openai_fim_compatible("claude3.5-haiku", "claude-3-5-haiku-20241022"),
+          openai_fim_compatible = minuet.openai_fim_compatible,
         },
       })
     end,
---
+++ lua/vars/minuet-ai.lua
@@ -0,0 +1,63 @@
+local secret = require("vars.secret")
+
+local default_stop = {
+  "<|endoftext|>",
+  "<|fim_prefix|>",
+  "<|fim_middle|>",
+  "<|fim_suffix|>",
+  "<|fim_pad|>",
+  "<|repo_name|>",
+  "<|file_sep|>",
+  "<|im_start|>",
+  "<|im_end|>",
+  "/src/",
+  "#- coding: utf-8",
+  "```",
+  "\n\n",
+}
+
+local default_optional = {
+  max_tokens = 4096,
+  temperature = 0.01,
+  top_p = 0.2,
+  provider = {
+    sort = "throughput",
+  },
+  num_predict = 4096,
+  num_ctx = 8192,
+  stop = default_stop,
+}
+
+local function openai_fim_compatible(name, end_point, api_key, model, stream, optional)
+  return {
+    name = name,
+    end_point = end_point,
+    api_key = function()
+      return api_key
+    end,
+    model = model,
+    stream = stream,
+    optional = optional,
+  }
+end
+
+local worklink_endpoint = "https://worklink.yealink.com/llmproxy/v1/completions"
+local worklink_api_key = secret.worklink_llm
+local function worklink_openai_fim_compatible(name, model)
+  return openai_fim_compatible(name, worklink_endpoint, worklink_api_key, model, true, default_optional)
+end
+
+local aihubmix_endpoint = "https://aihubmix.com"
+local aihubmix_api_key = secret.aihubmix
+local function aihubmix_openai_fim_compatible(name, model)
+  return openai_fim_compatible(name, aihubmix_endpoint, aihubmix_api_key, model, true, default_optional)
+end
+
+local openai_fim_compatible_map = {
+  worklink_qwen = worklink_openai_fim_compatible("qwen2.5-coder:32b", "qwen2.5-coder-32b-instruct"),
+  aihubmix_claude = aihubmix_openai_fim_compatible("claude3.5-haiku", "claude-3-5-haiku-20241022"),
+}
+
+return {
+  openai_fim_compatible = openai_fim_compatible_map["worklink_qwen"],
+}
