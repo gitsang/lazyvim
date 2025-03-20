0960eb64ae929b6edac0648aec1d4a8db2d959a7
sang <sang.chen@outlook.com>
2025-03-20 16:05:34 +0800

    update(auto): auto commit
    MODIFIED:
    	lua/plugins/ui.lua

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -12,7 +12,7 @@ return {
     "sphamba/smear-cursor.nvim",
     opts = {
       stiffness = 0.6,
-      trailing_stiffness = 0.5,
+      trailing_stiffness = 0.3,
       distance_stop_animating = 0.1,
     },
   },
