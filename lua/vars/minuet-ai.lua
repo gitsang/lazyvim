0bfac3da995291f3b00ae1737e0dd35b655efb20
sang <sang.chen@outlook.com>
2025-03-20 11:20:06 +0800

    update(auto): auto commit
    MODIFIED:
    	lua/vars/minuet-ai.lua

--- lua/vars/minuet-ai.lua
+++ lua/vars/minuet-ai.lua
@@ -41,7 +41,7 @@ local function openai_fim_compatible(name, end_point, api_key, model, stream, op
   }
 end
 
-local worklink_endpoint = "https://worklink.yealink.com/llmproxy/v1/completions"
+local worklink_endpoint = "http://openai-proxy.ops.yl.c8g.top:8888/llmproxy/v1/completions"
 local worklink_api_key = secret.worklink_llm
 local function worklink_openai_fim_compatible(name, model)
   return openai_fim_compatible(name, worklink_endpoint, worklink_api_key, model, true, default_optional)
@@ -53,7 +53,7 @@ local function aihubmix_openai_fim_compatible(name, model)
   return openai_fim_compatible(name, aihubmix_endpoint, aihubmix_api_key, model, true, default_optional)
 end
 
-local siliconflow_endpoint = "https://api.siliconflow.cn/v1/chat/completions"
+local siliconflow_endpoint = "https://api.siliconflow.cn/v1/completions"
 local siliconflow_api_key = secret.siliconflow
 local function siliconflow_openai_fim_compatible(name, model)
   return openai_fim_compatible(name, siliconflow_endpoint, siliconflow_api_key, model, true, default_optional)
@@ -70,8 +70,8 @@ local openai_fim_compatible_map = {
   worklink_claude_haiku = worklink_openai_fim_compatible("claude-3.5-haiku", "claude-3.5-haiku"),
   worklink_claude_sonnet = worklink_openai_fim_compatible("claude-3.7-sonnet", "claude-3.7-sonnet"),
   aihubmix_claude = aihubmix_openai_fim_compatible("claude3.5-haiku", "claude-3-5-haiku-20241022"),
-  aihubmix_qwen = aihubmix_openai_fim_compatible("qwen2.5-coder:32b-instruct", "Qwen/Qwen2.5-Coder-32B-Instruct"),
-  sliconflow_qwen = siliconflow_openai_fim_compatible("qwen2.5-coder:7b-instruct", "Qwen/Qwen2.5-Coder-7B-Instruct"),
+  aihubmix_qwen = aihubmix_openai_fim_compatible("qwen2.5-coder:32b", "Qwen/Qwen2.5-Coder-32B-Instruct"),
+  sliconflow_qwen = siliconflow_openai_fim_compatible("qwen2.5-coder:32b", "Qwen/Qwen2.5-Coder-32B-Instruct"),
   ollama_qwen = ollama_openai_fim_compatible("qwen2.5-coder:7b", "qwen2.5-coder:7b"),
 }
 
