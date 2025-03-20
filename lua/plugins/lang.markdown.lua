eaa44bb43d2bdb36c0e4735e6c887b1fc623dc28
sang <sang.chen@outlook.com>
2025-03-20 15:03:33 +0800

    update(auto): auto commit
    MODIFIED:
    	lua/plugins/lang.markdown.lua

--- lua/plugins/lang.markdown.lua
+++ lua/plugins/lang.markdown.lua
@@ -30,13 +30,7 @@ return {
         },
       },
       anti_conceal = {
-        -- This enables hiding any added text on the line the cursor is on.
         enabled = true,
-        -- Which elements to always show, ignoring anti conceal behavior. Values can either be
-        -- booleans to fix the behavior or string lists representing modes where anti conceal
-        -- behavior will be ignored. Valid values are:
-        --   head_icon, head_background, head_border, code_language, code_background, code_border
-        --   dash, bullet, check_icon, check_scope, quote, table_border, callout, link, sign
         ignore = {
           code_language = true,
           code_background = true,
