8641f138f34af4db5dd4726747ea042c8c072ee2
sang <sang.chen@outlook.com>
2025-04-21 19:12:45 +0800

    Refactor prompts library directory structure

--- lua/vars/codecompanion/prompt_library.lua
+++
@@ -1,6 +0,0 @@
-return {
-  ["Explain"] = require("vars.codecompanion.prompts.explain"),
-  ["Analyze"] = require("vars.codecompanion.prompts.analyze"),
-  ["Fix"] = require("vars.codecompanion.prompts.fix"),
-  ["Translate"] = require("vars.codecompanion.prompts.translate"),
-}
---
+++ lua/vars/codecompanion/prompt_library/init.lua
@@ -0,0 +1,6 @@
+return {
+  ["Explain"] = require("vars.codecompanion.prompt_library.explain"),
+  ["Analyze"] = require("vars.codecompanion.prompt_library.analyze"),
+  ["Fix"] = require("vars.codecompanion.prompt_library.fix"),
+  ["Translate"] = require("vars.codecompanion.prompt_library.translate"),
+}
