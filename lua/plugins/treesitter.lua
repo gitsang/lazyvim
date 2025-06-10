bd6688e757398e3f199b98ce370836e0090d1db2
sang <sang.chen@outlook.com>
2025-06-10 16:01:22 +0800

    Swap Markdown query paths for Treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -12,16 +12,17 @@ end
 
 local HOME = os.getenv("HOME")
 
-vim.treesitter.query.set(
-  "markdown",
-  "highlights",
-  read_scm_file(HOME .. "/.config/nvim/lua/plugins/treesitter/queries/markdown/highlights.scm")
-)
-
+-- https://github.com/MeanderingProgrammer/render-markdown.nvim/discussions/370#discussioncomment-12555901
 vim.treesitter.query.set(
   "markdown_inline",
   "highlights",
   read_scm_file(HOME .. "/.config/nvim/lua/plugins/treesitter/queries/markdown_inline/highlights.scm")
+)
+
+vim.treesitter.query.set(
+  "markdown",
+  "highlights",
+  read_scm_file(HOME .. "/.config/nvim/lua/plugins/treesitter/queries/markdown/highlights.scm")
 )
 
 return {}
