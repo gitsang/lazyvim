7eaaee98a0ed50748db67c54e266ba6176ecbe0c
sang <sang.chen@outlook.com>
2025-09-04 15:59:20 +0800

    Refactor Yanky key mappings for Shada commands

--- lua/plugins/yanky.lua
+++ lua/plugins/yanky.lua
@@ -3,24 +3,6 @@ return {
   opts = {},
   dependencies = { "folke/snacks.nvim" },
   keys = {
-    {
-      "p",
-      function()
-        vim.cmd("rshada")
-        local keys = vim.api.nvim_replace_termcodes("<Plug>(YankyPutAfter)", true, false, true)
-        vim.api.nvim_feedkeys(keys, "n", false)
-      end,
-      desc = "Yanky PutAfter",
-    },
-    {
-      "<leader>p",
-      function()
-        vim.cmd("rshada")
-        ---@diagnostic disable-next-line: undefined-field
-        Snacks.picker.yanky()
-      end,
-      desc = "Open Yank History",
-    },
     { "<leader>SW", "<cmd>wshada<cr>", desc = "Write Shada" },
     { "<leader>SR", "<cmd>rshada<cr>", desc = "Read Shada" },
   },
